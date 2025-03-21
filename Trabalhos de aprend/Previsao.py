63a47cf9aca77cabace2fc4e724275ed760c462f
1711299-lucas <lucasevora027@gmail.com>
2025-03-21 13:08:40 +0000

    up

--- Trabalhos de aprend/Previsao.py
+++ Trabalhos de aprend/Previsao.py
@@ -14,18 +14,18 @@ print(fmap_data)
 flist_data =    np.array([list(fmap_data)])
 
 print(flist_data)
-data1 = pd.read_csv("Abalone_G1_predictor",sep=";")
+data1 = pd.read_csv("abalone.data",sep=";")
 data2 = data1.iloc[:, :11]
 data_preparation = pd.DataFrame(flist_data, columns=data1.columns[:11])
 out=data2
 for x in out:
     print(x,data_preparation[x].values)
-loaded_model = p1.load(open('Abalone_G1_predictor', 'rb'))
+loaded_model = p1.load(open('abalone.data', 'rb'))
 y_pred=loaded_model.predict(flist_data)
 print("wine quality",int(y_pred))
 
 if int(y_pred)  ==  data1_y[0]:
-    print("Abalone_G1_predictor(X)=", int(y_pred), "==", data1_y[0])
+    print("abalone.data(X)=", int(y_pred), "==", data1_y[0])
 else:
-    print("Abalone_G1_predictor(X)=", int(y_pred), "==", data1_y[0])
+    print("abalone.data(X)=", int(y_pred), "==", data1_y[0])
 
