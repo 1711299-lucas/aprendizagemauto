c01c8068ceb7d012674d3515bfcc15fa54e82970
1711299-lucas <lucasevora027@gmail.com>
2025-03-28 11:34:37 +0000

    update

--- Trabalhos de aprend/Dados de  treino.py
+++ Trabalhos de aprend/Dados de  treino.py
@@ -6,10 +6,11 @@ from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 column_names = ['comprimento', 'diametro', 'altura', 'peso_inteiro', 'peso_sem_casca', 'peso_viceras', 'peso_concha', 'n_aneis']  # input
-dataset = pd.read_csv('abalone.data', header=None, names=column_names)
+dataset = pd.read_csv('abalone.data', header=None, names=column_names,sep=",")
 train_data = dataset[:3133]
 data_X = train_data.iloc[:, 1:8]
 data_Y = train_data.iloc[:, 8:9]
+
 print(data_X)
 print(data_Y)
 regr = linear_model.LinearRegression()
--- Trabalhos de aprend/Teste.py
+++ Trabalhos de aprend/Teste.py
@@ -1,34 +1,16 @@
+import data
 import matplotlib.pyplot as plt
 import pickle as p1
 import numpy as np
 import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
-
-dataset = pd.read_csv("abalone.data",sep=";") ## -- ou -- sep=",")
-
-evaluation_data =   dataset[1044:]
-data_X=evaluation_data.iloc[:,8:9]
-data_Y=evaluation_data.iloc[:,9:10]
-
+evaluation_data=datasets[1001:]
+data_X=evaluation_data.iloc[:,0:11]
+data_Y=evaluation_data.iloc[:,11:12]
 print(type(evaluation_data))
 print(type(data_X))
-
-loaded_model = p1.load(open('abalone.data', 'rb'))
+loaded_model = p1.load(open('../white-wine_quality_predictor', 'rb'))
 print("Coefficients: \n", loaded_model.coef_)
-y_pred = loaded_model.predict(data_X)
-z_pred = y_pred - data_Y.values
-
-right = 0
-wrong = 0
-total = 0
-
-for x in z_pred:
-    z = int(x)
-    total += 1
-    if z == 0:
-        right += 1
-    else:
-        wrong += 1
-print("accuracy1= ", right / total if total > 0 else 0, "accuracy2= ", wrong / total if total > 0 else 0)
-
+y_pred=loaded_model.predict(data_X)
+z_pred=y_pred-data_Y
