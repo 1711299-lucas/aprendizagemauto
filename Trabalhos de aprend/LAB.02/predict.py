4793f6d2b4a1933d0cddd18d73b86e0b0570cc20
1711299-lucas <lucasevora027@gmail.com>
2025-05-25 19:36:41 +0100

    UP

--- Trabalhos de aprend/LAB.02/predict.py
+++ Trabalhos de aprend/LAB.02/predict.py
@@ -7,11 +7,11 @@ from sklearn import datasets, linear_model
 
 # Plot a predict point
 sns.scatterplot(
-x=(X[1,0]+X[43,0])/2,
-y=(X[1,1]+X[43,1])/2,
+x=(X[1,0]+ X[43,0])/2,
+y=(X[1,1]+ X[43,1])/2,
 marker="X",
 s=90,
-hue=optdigits.target_names[y],
+hue=optdigits.names[y],
 palette=cmap_bold,
 alpha=1.0,
 edgecolor="w",)
